ae2d4fbc986718df5d96a1a9a766b74c1898ef39
PhilDaiguille <philippe.delente@gmail.com>
2022-09-20 11:03:21 +0200

    First Commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+	/*APP*/
+	console.log("charged")
+
+	console.log(React)
+	const MyComponent = () => ( <p> Oui </p> );
+
+	ReactDOM.render(<MyComponent />, document.getElementById("react-goes-here"))
+	
+});
